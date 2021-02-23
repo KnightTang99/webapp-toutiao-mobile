17b0ab853f12a39f022329aad75b9a3afd400548
itcast <1521086170@qq.com>
2021-02-23 19:51:47 +0800

    init-project

--- src/main.js
+++ src/main.js
@@ -2,7 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+// 加载全局样式
+import './style/index.less'
+// 加载Vant
+import Vant from 'vant'
+import 'vant/lib/index.css'
+// 加载amfe-flexible
+import 'amfe-flexible'
+Vue.use(Vant)
 Vue.config.productionTip = false
 
 new Vue({
---
+++ src/views/login/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>黑马头条</div>
+</template>
+<script>
+export default {
+  name: 'loginPage',
+  data() {
+    return {}
+  },
+  created() {},
+  mounted() {},
+  methods: {}
+}
+</script>
+<style lang="less" scoped></style>
